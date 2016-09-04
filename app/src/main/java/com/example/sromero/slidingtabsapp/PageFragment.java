ed784f34507d407cbb34b05bd451ddc082e0b231
Sergio Romero <sergiorom92@gmail.com>
2016-09-04 10:49:30 -0500

    Proyecto Aplicación Skin UV Health

---
+++ app/src/main/java/com/example/sromero/slidingtabsapp/PageFragment.java
@@ -0,0 +1,73 @@
+package com.example.sromero.slidingtabsapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+/**
+ * Created by sromero on 27/08/2016.
+ */
+public class PageFragment extends Fragment {
+
+    private WebView wv1;
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static PageFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    // Inflate the fragment layout we defined above for this fragment
+    // Set the associated text for the title
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+        tvTitle.setText("Fragment #" + mPage);
+
+        switch (mPage) {
+            case 2:
+                view = inflater.inflate(R.layout.activity_sun_burn, container, false);
+                wv1 = (WebView) view.findViewById(R.id.webView);
+                wv1.setWebViewClient(new MyBrowser());
+
+
+                String url = "http:sunburnmap.com/es";
+
+                wv1.getSettings().setLoadsImagesAutomatically(true);
+                wv1.getSettings().setJavaScriptEnabled(true);
+                wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                wv1.loadUrl(url);
+
+
+        }
+
+        return view;
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
+
+
